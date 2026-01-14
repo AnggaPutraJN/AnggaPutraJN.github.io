80124ddaeec4f2d8c570cdc880c45932dcc390d6
AnggaPutraJN <35292974+AnggaPutraJN@users.noreply.github.com>
2026-01-14 23:56:09 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'fintrack-v1.0';
 
 // File yang akan di-cache
 const urlsToCache = [
-  'keuangan.html',
+  '/keuangan.html',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css',
   'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap',
@@ -55,7 +55,7 @@ self.addEventListener('fetch', event => {
           .catch(() => {
             // Fallback jika offline
             if (event.request.headers.get('accept').includes('text/html')) {
-              return caches.match('keuangan.html');
+              return caches.match('/keuangan.html');
             }
           });
       })
